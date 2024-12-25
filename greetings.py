9b709b0cc2aec345ddc29b47fdee616593912249
Blazhko Kirill <kirill7-07@mail.ru>
2024-12-25 10:14:25 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Кирилл'
+title = 'Расписание'
+content = 'Расписание на учебный год'
+status = 'выполняется'
+created_date = '12-12-2024'
+issue_date = '12-12-2025'
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date)
+print(issue_date)
